fa28e771b711db55bcc60b9b6696f05448a58bb9
Andrew <artemdanilenko1997@gmail.com>
2025-08-15 19:30:32 +0300

    add: sign in | sign up functionality

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { Alert } from "react-native";
+
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { createUser } from "../util/auth";
-
+import { AuthContext } from "../store/auth-context";
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const authCtx = useContext(AuthContext);
+
   async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
-    await createUser(email, password);
-    setIsAuthenticating(false);
+    try {
+      const token = createUser(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not create user, please check your input and try again later."
+      );
+      setIsAuthenticating(false);
+    }
   }
 
   if (isAuthenticating) {
